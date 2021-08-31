9fcffa7be3cce0bf8b3ba36d6b23f30c8fc908bc
Karen Lundgren <karen.e.lundgren@gmail.com>
2021-08-30 19:41:11 -0600

    Models set up, tests passing

--- app/models/works_cited/contributor.rb
+++ app/models/works_cited/contributor.rb
@@ -1,5 +1,5 @@
 module WorksCited
   class Contributor < ApplicationRecord
-    belongs_to :works_cited_citation, inverse_of: :works_cited_contributor
+    belongs_to :works_cited_citation, inverse_of: :works_cited_contributors, class_name: 'WorksCited::Citation'
   end
 end
--- spec/models/works_cited/contributor_spec.rb
+++ spec/models/works_cited/contributor_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 module WorksCited
   RSpec.describe Contributor, type: :model do
     describe 'Relationships' do
-      it { should belong_to(:works_cited_citations) }
+      it { should belong_to(:works_cited_citation) }
     end
   end
 end
