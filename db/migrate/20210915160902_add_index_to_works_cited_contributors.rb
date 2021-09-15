ac928a51e9bce2b837a95400659945862da6cf9a
Karen Lundgren <karen.e.lundgren@gmail.com>
2021-09-15 10:24:32 -0600

    Fixed length in index

--- db/migrate/20210915160902_add_index_to_works_cited_contributors.rb
+++ db/migrate/20210915160902_add_index_to_works_cited_contributors.rb
@@ -1,5 +1,5 @@
 class AddIndexToWorksCitedContributors < ActiveRecord::Migration[6.1]
   def change
-    add_index :works_cited_contributors, [:last, :first, :middle, :suffix, :handle], name: :index_citation_name
+    add_index :works_cited_contributors, [:last, :first, :middle, :suffix, :handle], name: :index_citation_name, length: 100
   end
 end
